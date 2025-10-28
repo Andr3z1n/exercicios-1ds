1b9e56e091ea96e825097ffb5b15a3bfda6c46ca
Andre Ryan <andre.soares12@aluno.ce.gov.br>
2025-10-28 14:31:33 +0000

    feito!

--- 4-bimestre/ex04-array/index.js
+++ 4-bimestre/ex04-array/index.js
@@ -2,4 +2,4 @@
 let materias = ["logica de programação", "historia", "matemática", "português"]
 console.log(materias[0], materias[3], materias.length)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
-module.exports = { materias }
+l

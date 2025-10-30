2a3d5c5d156db801555f56a5e9539366a1421ad1
Andre Ryan <andre.soares12@aluno.ce.gov.br>
2025-10-30 17:39:24 +0000

    feito!

--- 4-bimestre/ex09-criando-um-objeto/index.js
+++ 4-bimestre/ex09-criando-um-objeto/index.js
@@ -1,4 +1,10 @@
 // CRIE UMA LÓGICA AQUI
-
+let produto = {
+    nome: "Estojo",
+    preco: 10,
+    estoque: "30",
+    categoria: "objeto"
+}
+console.log(produto)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { produto }
